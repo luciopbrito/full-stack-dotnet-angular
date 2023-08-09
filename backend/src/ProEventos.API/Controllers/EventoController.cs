b91d8486587711c3795c2041e9528fa40f826580
Lucio Brito <luciopereirabritoo@gmail.com>
2023-08-08 23:35:02 -0300

    feature: event model;

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers;
 
@@ -6,14 +7,40 @@ namespace ProEventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
+    public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() {
+            EventoId = 1,
+            Tema = "Angular 11 e .NET 5",
+            Local = "Belo Horizonte",
+            Lote = "1º Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString(),
+            ImagemURL = "foto.png"
+            },
+            new Evento() {
+            EventoId = 2,
+            Tema = "Angular e Suas Novidades",
+            Local = "São Paulo",
+            Lote = "2º Lote",
+            QtdPessoas = 350,
+            DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+            ImagemURL = "foto1.png"
+            }
+        };
     public EventoController()
     {
 
     }
 
     [HttpGet(Name = "GetEvento")]
-    public string Get()
+    public IEnumerable<Evento> Get()
     {
-        return "value";
+        return _evento;
+    }
+
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetById(int id)
+    {
+        return _evento.Where(e => e.EventoId == id);
     }
 }
