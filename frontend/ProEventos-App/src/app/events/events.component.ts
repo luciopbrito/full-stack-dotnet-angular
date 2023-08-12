60e5fd86879ca7f40e2fbebed7741173a5401536
Lucio Brito <luciopereirabritoo@gmail.com>
2023-08-12 14:35:00 -0300

    feature: using ng for;

--- frontend/ProEventos-App/src/app/events/events.component.ts
+++ frontend/ProEventos-App/src/app/events/events.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent {
-
+  public events: any = [
+    {
+      Tema: 'Angular',
+      Local: 'Belo Horizonte',
+    },
+    {
+      Tema: '.NET 7',
+      Local: 'Belo Horizonte',
+    },
+    {
+      Tema: 'Angular e Suas Novidades',
+      Local: 'Belo Horizonte',
+    },
+  ];
 }
