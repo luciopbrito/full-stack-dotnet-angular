8c31f7bc37fd96bfb59de4bf342bd4d6c800a014
Lucio Brito <luciopereirabritoo@gmail.com>
2023-09-10 13:16:16 -0300

    feature: filtering events;

--- frontend/ProEventos-App/src/app/events/events.component.ts
+++ frontend/ProEventos-App/src/app/events/events.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  public events: any = [];
+  public events: Event[] = [];
+  public eventsFilters: Event[] = [];
   widthImg: number = 150;
   marginImg: number = 2;
   showImage: boolean = false;
-  filter: string = '';
+  private _filter: string = '';
+
+  public get filter() {
+    return this._filter;
+  }
+
+  public set filter(value: string) {
+    this._filter = value;
+    this.eventsFilters = this.filter ? this.listFilter(this.filter) : this.events;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -23,10 +33,35 @@ export class EventsComponent implements OnInit {
     this.showImage = !this.showImage;
   }
 
+  public listFilter(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      e => e.dataEvento.toString().toLowerCase().indexOf(filterBy) !== -1 ||
+      e.eventoId.toString().toLowerCase().indexOf(filterBy) !== -1 ||
+      e.imagemURL.toString().toLowerCase().indexOf(filterBy) !== -1 ||
+      e.local.toString().toLowerCase().indexOf(filterBy) !== -1 ||
+      e.qtdPessoas.toString().toLowerCase().indexOf(filterBy) !== -1 ||
+      e.tema.toString().toLowerCase().indexOf(filterBy) !== -1
+    )
+  }
+
   public getEvents(): void {
-    this.http.get('http://localhost:5134/api/Eventos').subscribe(
-      response => this.events = response,
+    this.http.get<Event[]>('http://localhost:5134/api/Eventos').subscribe(
+      response => {
+        this.events = response;
+        this.eventsFilters = this.events;
+      },
       error => console.log(error)
     );
   }
+}
+
+type Event = {
+  eventoId: number,
+  local: string,
+  dataEvento: string,
+  tema: string,
+  qtdPessoas: number,
+  lote: string,
+  imagemURL: string
 }
