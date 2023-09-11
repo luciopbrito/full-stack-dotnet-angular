756756e1ef5b1d9ce41aa77cb2e89350d7d7e510
Lucio Brito <luciopereirabritoo@gmail.com>
2023-09-11 09:50:10 -0300

    feature: persistence organization;

---
+++ backend/src/ProEventos.Persistence/Contracts/IGeneralPersist.cs
@@ -0,0 +1,11 @@
+namespace ProEventos.Persistence.Contracts
+{
+    public interface IGeneralPersist
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(T[] entityArray) where T : class;
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ backend/src/ProEventos.Persistence/EventPersist.cs
@@ -0,0 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+using ProEventos.Persistence.Contracts;
+
+namespace ProEventos.Persistence
+{
+    public class EventPersist : IEventPersist
+    {
+        private readonly ProEventosContext _context;
+
+        public EventPersist(ProEventosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Event[]> GetAllEventsAsync(string theme, bool includeSpeakers = false)
+        {
+            IQueryable<Event> query = _context.Events
+                .Include(e => e.Batches)
+                .Include(e => e.SocialMedias);
+
+            if (includeSpeakers)
+            {
+                query = query.Include(e => e.SpeakerEvents).ThenInclude(se => se.Speaker);
+            }
+
+            query = query.OrderBy(e => e.Id);
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Event[]> GetAllEventsByThemeAsync(string theme, bool includeSpeakers = false)
+        {
+            IQueryable<Event> query = _context.Events
+                .Include(e => e.Batches)
+                .Include(e => e.SocialMedias);
+
+            if (includeSpeakers)
+            {
+                query = query.Include(e => e.SpeakerEvents).ThenInclude(se => se.Speaker);
+            }
+
+            query = query.OrderBy(e => e.Id)
+                .Where(e => e.Theme.ToLower().Contains(theme.ToLower()));
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Event> GetEventByIdAsync(int eventId, bool includeSpeakers = false)
+        {
+            IQueryable<Event> query = _context.Events
+               .Include(e => e.Batches)
+               .Include(e => e.SocialMedias);
+
+            if (includeSpeakers)
+            {
+                query = query.Include(e => e.SpeakerEvents).ThenInclude(se => se.Speaker);
+            }
+
+            query = query.OrderBy(e => e.Id)
+                .Where(e => e.Id == eventId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+    }
+}
