c491ee71c64f838c2e9a4d05ac77a11fb7e57729
Lúcio Brito <luciopereirabritoo@gmail.com>
2026-02-19 15:14:51 -0300

    Feat: Add event service
    To provide event information through the application, it is a good
    practice use a specific service.

---
+++ frontend/ProEventos-App/src/app/services/events.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Event } from '../models/Event.interface';
+
+@Injectable()
+export class EventsService {
+  /**
+   * @description property to get host URL to use as based URL for all methods
+   * inside here.
+   * @default `http://localhost:5134/api/events`
+   */
+  private _hostUrl = 'http://localhost:5134/api/events'
+
+  constructor(
+    private _httpClient: HttpClient,
+  ) { }
+
+  getAllEvents() {
+    return this._httpClient.get<Event[]>(`${this._hostUrl}`);
+  }
+}
